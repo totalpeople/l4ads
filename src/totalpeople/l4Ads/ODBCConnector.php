33fa6cfcd9d68e3ab4ed60ee7d8ed8307298bbda
MacAdmin <macadmin@macbook-pro.default>
2014-03-17 11:43:27 +0000

    First commit

---
+++ src/totalpeople/l4Ads/ODBCConnector.php
@@ -0,0 +1,25 @@
+<?php namespace totalpeople\l4Ads;
+
+use Illuminate\Database\Connectors\Connector;
+use Illuminate\Database\Connectors\ConnectorInterface;
+
+class ODBCConnector extends Connector implements ConnectorInterface {
+
+	/**
+	 * Establish a database connection.
+	 *
+	 * @param  array  $options
+	 * @return PDO
+	 */
+	public function connect(array $config)
+	{
+		$options = $this->getOptions($config);
+
+        $dsn = array_get($config, 'dsn');
+
+		return $this->createConnection($dsn, $config, $options);
+	}
+
+	
+
+}
---
+++ src/totalpeople/l4Ads/ODBCQueryGrammar.php
@@ -0,0 +1,9 @@
+<?php namespace totalpeople\l4Ads;
+
+use Illuminate\Database\Query\Grammars\Grammar;
+
+class ODBCQueryGrammar extends Grammar {
+
+
+
+}
